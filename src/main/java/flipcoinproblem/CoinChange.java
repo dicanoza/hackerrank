520502e5acff17ced7926deb492762010d0bb2b8
Diogenes Correa Canoza <dicanoza@gmail.com>
2017-02-19 23:26:18 -0300

    FlipCoin problem

---
+++ src/main/java/flipcoinproblem/CoinChange.java
@@ -0,0 +1,25 @@
+package flipcoinproblem;
+
+import static java.util.Arrays.fill;
+
+class CoinChange {
+	static long countWays(int coins[], int m, int n) {
+		long[] table = new long[n + 1];
+		fill(table, 0);
+		table[0] = 1;
+		for (int i = 0; i < m; i++) {
+			for (int j = coins[i]; j <= n; j++) {
+				table[j] += table[j - coins[i]];
+			}
+		}
+
+		return table[n];
+	}
+
+	public static void main(String args[]) {
+		int arr[] = { 2, 3, 5, 6 };
+		int m = arr.length;
+		int n = 10;
+		System.out.println(countWays(arr, m, n));
+	}
+}
